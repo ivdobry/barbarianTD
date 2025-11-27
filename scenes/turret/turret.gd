e28b9ac87d73a69dcae9c5e33792913e02adcb1e
ivdobry <ivan@rzilient.club>
2025-11-27 14:07:51 +0100

    Add turret projectiles.

---
+++ scenes/turret/turret.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@export var projectile: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var shot = projectile.instantiate()
+	add_child(shot)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
