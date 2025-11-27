fd8386133b138c3667ac78ad478d4f737e11724b
ivdobry <ivan@rzilient.club>
2025-11-27 15:59:37 +0100

    Add turrent range and enemy health.

--- scenes/turret/projectile.gd
+++ scenes/turret/projectile.gd
@@ -6,3 +6,11 @@ var direction := Vector3.FORWARD
 
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta 
+
+func _on_timer_timeout() -> void:
+	queue_free()
+
+func _on_area_entered(area: Area3D) -> void:
+	if area.is_in_group("enemy_area"):
+		area.get_parent().current_health -= 25
+		queue_free()
--- scenes/turret/turret.gd
+++ scenes/turret/turret.gd
@@ -1,13 +1,35 @@
 extends Node3D
 
 @export var projectile: PackedScene
+@export var turret_range := 10.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+@onready var turret_top: MeshInstance3D = $TurretBase/TurretTop
+
+var enemy_path: Path3D
+var target: PathFollow3D
+
+func _physics_process(_delta: float) -> void:
+	target = _find_best_target()
+	if target:
+		look_at(target.global_position, Vector3.UP, true)
+
+func _on_timer_timeout() -> void:
+	if !target:
+		return
+		
 	var shot = projectile.instantiate()
 	add_child(shot)
+	shot.global_position = turret_top.global_position
+	shot.direction = global_transform.basis.z
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _find_best_target() -> PathFollow3D:
+	var best_target = null
+	var best_progress := 0
+	for enemy in enemy_path.get_children():
+		var distance = global_position.distance_to(enemy.global_position)
+		if enemy is PathFollow3D and distance <= turret_range:
+			if enemy.progress > best_progress:
+				best_target = enemy
+				best_progress = enemy.progress
+		
+	return best_target
